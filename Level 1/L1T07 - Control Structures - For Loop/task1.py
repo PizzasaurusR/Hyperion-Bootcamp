d6a8c3feeaf517fd62dbc371e43ec1c2068f6cb9
PizzasaurusR <ramizissel@gmail.com>
2024-06-23 14:28:17 +0200

    Comments for L1T07 - task1.py updated to follow PEP8 style guide.

--- Level 1/L1T07 - Control Structures - For Loop/task1.py
+++ Level 1/L1T07 - Control Structures - For Loop/task1.py
@@ -1,14 +1,41 @@
+'''
+1 - Get user input for max number of lines
+2 - Create a var to track the midpoint of the shape. This is the longest
+    line of the shape.
+    Using math.ceil to ensure that the number is whole, no matter the 
+    amount of lines.
+3 - The line of stars (*) is created based on the midpoint length.
+4 - Starting from 1 till the midpoint, increase count and print a slice
+    of the line of stars based on the number that count currently is.
+5 - From the midpoint on, print stars less the the difference of 
+    max_num_lines and count.
+6 - Increase count to keep track of number of printed lines but 
+    increases after the print as it will have been increased in the 
+    first if loop.
+'''
+
 import math
+
 max_num_lines = int(input("Please enter the number of lines: "))
-count = 0 #track total number of lines printed
-line_size = math.ceil(max_num_lines / 2) #create a var to track the mid point of the shape ie the longest part of the shape. Using math.ceil to make sure that the number is whole no matter the amount of lines
-stars = "*" * line_size #declare stars to be printed
+
+# Track total number of lines printed.
+count = 0 
+
+# Create a var to track the mid point of the shape. 
+line_size = math.ceil(max_num_lines / 2) 
+
+# Declare stars to be printed
+stars = "*" * line_size 
 
 for i in range (0 ,max_num_lines):
     if count < line_size:
-        count += 1 #increase count to keep track of number of lines printed
-        print(stars[0:count]) #print a slice of stars based on what number is currently selected
+        # Increase count to keep track of num lines printed
+        count += 1 
+        # Print a slice of stars based on what num is currently selected
+        print(stars[0:count]) 
     
     elif (count >= line_size & count <= max_num_lines):
-        print(stars[0:(max_num_lines - count)]) #print stars less the number of lines that max_num_lines is greater than count
-        count += 1 #increase count to keep track of number of printed lines but increases after the print as it will have been increased in the first if loop
+        # Print Stars
+        print(stars[0:(max_num_lines - count)]) 
+        count += 1 
+        
